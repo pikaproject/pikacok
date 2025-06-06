31d002bd4c6147c6f65a83b10b06ffd6298f3c68
pikachu <aenulr97@gmail.com>
2025-06-06 19:35:37 +0800

    r

--- misskaty/vars.py
+++ misskaty/vars.py
@@ -70,14 +70,13 @@ FORWARD_FROM_CHAT_ID = list(
     {
         int(x)
         for x in environ.get(
-            "FORWARD_FROM_CHAT_ID",
-            "-1001128045651 -1001455886928 -1001686184174",
+            "FORWARD_FROM_CHAT_ID"
         ).split()
     }
 )
 # Forward To Chat ID
 FORWARD_TO_CHAT_ID = list(
-    {int(x) for x in environ.get("FORWARD_TO_CHAT_ID", "-1001210537567").split()}
+    {int(x) for x in environ.get("FORWARD_TO_CHAT_ID").split()}
 )
 FORWARD_FILTERS = list(set(environ.get("FORWARD_FILTERS", "video document").split()))
 BLOCK_FILES_WITHOUT_EXTENSIONS = bool(
