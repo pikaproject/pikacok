72455f2cad394b69c52fb7469509e4b010a63f3c
pikachu <aenulr97@gmail.com>
2025-06-16 01:48:03 +0800

    a

--- misskaty/plugins/auto_kick.py
+++ misskaty/plugins/auto_kick.py
@@ -23,7 +23,7 @@ DEFAULT_KICK_TIME_MINUTES = int(os.getenv("DEFAULT_KICK_TIME_HOURS", "1"))
 #@app.adminsOnly("can_restrict_members")
 async def handle_autokick(client: Client, ctx: Message) -> "Message":
     chat_type = ctx.chat.type.value
-    if not await admins_in_chat(ctx):
+    if ctx.from_user.id not in (await list_admins(ctx.chat.id)):
         return await ctx.reply("❌ Anda tidak memiliki izin untuk menggunakan perintah ini.")
     if chat_type == "private":
         return await ctx.reply("Perintah ini hanya bisa digunakan di grup atau channel.")
